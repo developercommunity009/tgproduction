75f7c747104bc803b0fd2d48917613e7d6f9e522
developercommunity009@gmail.com <developercommunity009@gmail.com>
2024-09-20 10:57:36 +0300

    fridayo

--- client/src/Component/Pagination.jsx
+++ client/src/Component/Pagination.jsx
@@ -4,12 +4,12 @@ import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 const Pagination = ({ currentPage, onPageChange }) => {
   return (
-    <div className="flex justify-center items-center space-x-4 mt-4">
+    <div className="flex justify-center items-center space-x-12 mt-4">
       {/* Previous Page Button */}
       <button
         onClick={() => onPageChange(currentPage - 1)}
         disabled={currentPage === 1}
-        className="flex items-center justify-center p-2 rounded-full bg-white border-2 border-[#5cdbd4] shadow-lg hover:scale-105 transition-transform duration-300 ease-in-out disabled:bg-gray-200 disabled:cursor-not-allowed"
+        className="flex items-center justify-center p-1 rounded-full bg-white border-2 border-[#5cdbd4] shadow-lg hover:scale-105 transition-transform duration-300 ease-in-out disabled:bg-gray-200 disabled:cursor-not-allowed"
       >
         <ArrowBackIosNewIcon className="text-[#20807b]" />
       </button>
@@ -20,7 +20,7 @@ const Pagination = ({ currentPage, onPageChange }) => {
       {/* Next Page Button */}
       <button
         onClick={() => onPageChange(currentPage + 1)}
-        className="flex items-center justify-center p-2 rounded-full bg-white border-2 border-[#5cdbd4] shadow-lg hover:scale-105 transition-transform duration-300 ease-in-out"
+        className="flex items-center justify-center p-1 rounded-full bg-white border-2 border-[#5cdbd4] shadow-lg hover:scale-105 transition-transform duration-300 ease-in-out"
       >
         <ArrowForwardIosIcon className="text-[#20807b]" />
       </button>
